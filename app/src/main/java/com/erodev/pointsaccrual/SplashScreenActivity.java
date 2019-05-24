43d8cde5aacdc81c430cc57b3ca5f95d54d579ac
erodrago <eric.ndira@gmail.com>
2019-05-24 18:25:31 +0300

    Added splash screen

--- app/src/main/java/com/erodev/pointsaccrual/SplashScreenActivity.java
+++ app/src/main/java/com/erodev/pointsaccrual/SplashScreenActivity.java
@@ -1,13 +1,39 @@
 package com.erodev.pointsaccrual;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
 
 public class SplashScreenActivity extends AppCompatActivity {
+
+    //Splash Timer
+    private static int SPLASH_TIME_OUT=1000;
+    private ImageView splashimage;
+    private Animation anim;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+
+        splashimage=findViewById(R.id.splashimgview);
+        anim = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.anim_scale_up );
+        splashimage.startAnimation(anim);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashScreenActivity.this,MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        },SPLASH_TIME_OUT);
     }
 }
+
